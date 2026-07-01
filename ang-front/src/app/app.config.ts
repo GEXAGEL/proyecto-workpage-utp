7c8bdd728caaeaff8bd664bf2c282979738a680b
Angelo Quispe <angeloqui12345@gmail.com>
2026-07-01 00:35:17 -0500

    Registrando AuthInterceptor en app.config

--- ang-front/src/app/app.config.ts
+++ ang-front/src/app/app.config.ts
@@ -1,14 +1,21 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClient, withInterceptorsFromDi, HTTP_INTERCEPTORS } from '@angular/common/http';
 
 import { routes } from './app.routes';
+import { AuthInterceptor } from './auth.interceptor'; // <-- Asegúrate de que la ruta a tu interceptor sea correcta
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    provideHttpClient()
+    // Reemplazamos el provideHttpClient() viejo por este configurado para interceptores:
+    provideHttpClient(withInterceptorsFromDi()), 
+    { 
+      provide: HTTP_INTERCEPTORS, 
+      useClass: AuthInterceptor, 
+      multi: true 
+    }
   ]
 };
